0d65a383f7dbd6a74cb6936ad0989351d2badfda
lipusipu44 <anilpatro044@gmail.com>
2025-03-12 12:54:01 +0530

    Only changed the indentation

--- cmd/api/json.go
+++ cmd/api/json.go
@@ -9,9 +9,9 @@ func writeJSON(w http.ResponseWriter, status int, data any) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(status)
 	/*
-	This encodes the data struct into JSON and writes it to the response body.
-	If encoding fails, it returns an error.
-	 */
+		This encodes the data struct into JSON and writes it to the response body.
+		If encoding fails, it returns an error.
+	*/
 	return json.NewEncoder(w).Encode(data)
 }
 
@@ -20,43 +20,43 @@ func writeJSON(w http.ResponseWriter, status int, data any) error {
 This function reads JSON data from an HTTP request payload
 and decodes it into the provided data structure.
 It also enforces a size limit and prevents unknown fields.
- */
+*/
 func readJSON(w http.ResponseWriter, r *http.Request, data any) error {
 	/*
-	This prevents large payloads from
-	overwhelming the server.
-	Any request body larger than ~1MB will be rejected.
-	 */
+		This prevents large payloads from
+		overwhelming the server.
+		Any request body larger than ~1MB will be rejected.
+	*/
 
-	maxBytes := 1_048_578m // 1 mb
+	maxBytes := 1_048_578 // 1 mb
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
 
 	//This initializes a decoder to read JSON from the request body.
 	decoder := json.NewDecoder(r.Body)
 	/*
-	If the JSON request contains fields
-	that are not defined in the data struct,
-	it rejects the request.
-	 */
+		If the JSON request contains fields
+		that are not defined in the data struct,
+		it rejects the request.
+	*/
 	decoder.DisallowUnknownFields()
 	/*
-	This attempts to decode the JSON into the data struct.
-	If there’s an error
-	(e.g., invalid JSON, unknown fields, or exceeding size limits),
-	it returns an error.
-	 */
+		This attempts to decode the JSON into the data struct.
+		If there’s an error
+		(e.g., invalid JSON, unknown fields, or exceeding size limits),
+		it returns an error.
+	*/
 	return decoder.Decode(data)
 }
 
 //writeJSONError
 /*
 This function is a helper for sending JSON-formatted error messages in HTTP responses.
- */
+*/
 func writeJSONError(w http.ResponseWriter, status int, message string) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	//Defines an anonymous struct with a single field, error, to hold the error message.
 	type envelope struct {
 		Error string `json:"error"` //for json error formatting
 	}
-	return writeJSON(w, status,envelope{Error: message} )
+	return writeJSON(w, status, envelope{Error: message})
 }
